be6b1f90a4446d46733c3b07486c540a257b9ced
Boneshock <jordi.spammail@gmail.com>
2026-03-25 11:18:23 +0100

    update versioned data

--- VersionedData.lua
+++ VersionedData.lua
@@ -4,36 +4,33 @@ local Private = select(2, ...)
 local VersionedData = {
     [120001] = {
         CRESTS = {
-            [0] = { shortName = "Valorstones", color = HEIRLOOM_BLUE_COLOR },
-            [1] = { shortName = "Weathered", color = UNCOMMON_GREEN_COLOR, achieve = 41886 },
-            [2] = { shortName = "Carved", color = RARE_BLUE_COLOR, achieve = 41887 },
-            [3] = { shortName = "Runed", color = ITEM_EPIC_COLOR, achieve = 41888 },
-            [4] = { shortName = "Gilded", color = ITEM_LEGENDARY_COLOR, achieve = 41892 },
+            [0] = { shortName = "Gold", color = GOLD_FONT_COLOR },
+            [1] = { shortName = "Adventurer", color = WHITE_FONT_COLOR, achieve = 61809 },
+            [2] = { shortName = "Veteran", color = UNCOMMON_GREEN_COLOR, achieve = 42767 },
+            [3] = { shortName = "Champion", color = RARE_BLUE_COLOR, achieve = 42768 },
+            [4] = { shortName = "Hero", color = ITEM_EPIC_COLOR, achieve = 42769 },
+            [5] = { shortName = "Myth", color = ITEM_LEGENDARY_COLOR, achieve = 42770 },
         },
         UPGRADE_TIERS = {
             {
-                name = "Explorer", minIlvl = 207, maxIlvl = 230, maxUpgrade = 6, color = ITEM_POOR_COLOR,
-                crestLevels = { [1] = 0, [4] = 0, [6] = nil }
+                name = "Adventurer", minIlvl = 220, maxIlvl = 237, maxUpgrade = 6, color = WHITE_FONT_COLOR,
+                crestLevels = { [1] = 1, [6] = nil }
             },
             {
-                name = "Adventurer", minIlvl = 220, maxIlvl = 243, maxUpgrade = 6, color = WHITE_FONT_COLOR,
-                crestLevels = { [1] = 0, [4] = 1, [6] = nil }
+                name = "Veteran", minIlvl = 233, maxIlvl = 250, maxUpgrade = 6, color = UNCOMMON_GREEN_COLOR,
+                crestLevels = { [1] = 2, [6] = nil }
             },
             {
-                name = "Veteran", minIlvl = 233, maxIlvl = 256, maxUpgrade = 6, color = UNCOMMON_GREEN_COLOR,
-                crestLevels = { [1] = 1, [4] = 2, [6] = nil }
+                name = "Champion", minIlvl = 246, maxIlvl = 263, maxUpgrade = 6, color = RARE_BLUE_COLOR,
+                crestLevels = { [1] = 3, [6] = nil }
             },
             {
-                name = "Champion", minIlvl = 246, maxIlvl = 269, maxUpgrade = 6, color = RARE_BLUE_COLOR,
-                crestLevels = { [1] = 2, [4] = 3, [6] = nil }
-            },
-            {
-                name = "Hero", minIlvl = 259, maxIlvl = 282, maxUpgrade = 6, color = ITEM_EPIC_COLOR,
-                crestLevels = { [1] = 3, [4] = 4, [6] = nil }
+                name = "Hero", minIlvl = 259, maxIlvl = 276, maxUpgrade = 6, color = ITEM_EPIC_COLOR,
+                crestLevels = { [1] = 4, [6] = nil }
             },
             {
                 name = "Myth", minIlvl = 272, maxIlvl = 289, maxUpgrade = 6, color = ITEM_LEGENDARY_COLOR,
-                crestLevels = { [1] = 4, [6] = nil }
+                crestLevels = { [1] = 5, [6] = nil }
             },
         },
     },
